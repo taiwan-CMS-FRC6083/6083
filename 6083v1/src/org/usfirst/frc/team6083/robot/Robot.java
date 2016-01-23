a2dbb7041fa5f7d2ba152fedbfcb2f925a657093
Lin410059 <flps4436@gmail.com>
2016-01-23 22:02:50 +0800

    新增相機

--- 6083v1/src/org/usfirst/frc/team6083/robot/Robot.java
+++ 6083v1/src/org/usfirst/frc/team6083/robot/Robot.java
@@ -5,6 +5,13 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.vision.AxisCamera;
+
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.DrawMode;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ShapeMode;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -36,6 +43,11 @@ public class Robot extends IterativeRobot {
     //SmartDashboard
     Preferences pref;
     
+    //camera
+    int session;
+    Image frame;
+    AxisCamera camera;
+    
     
     
     
@@ -49,6 +61,13 @@ public class Robot extends IterativeRobot {
         chooser.addObject("My Auto", customAuto);
         SmartDashboard.putData("Auto choices", chooser);
         pref.getDouble("SpeedControal", 5.0);
+        
+        
+        frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+
+        // open the camera at the IP address assigned. This is the IP address that the camera
+        // can be accessed through the web interface.
+        camera = new AxisCamera("10.1.91.100");
     }
     
 	/**
@@ -85,6 +104,24 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
+    	 /**
+         * grab an image from the camera, draw the circle, and provide it for the camera server
+         * which will in turn send it to the dashboard.
+       */
+         NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
+
+         while (isOperatorControl() && isEnabled()) {
+             camera.getImage(frame);
+             NIVision.imaqDrawShapeOnImage(frame, frame, rect,
+                     DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
+
+             CameraServer.getInstance().setImage(frame);
+
+             /** robot code here! **/
+             Timer.delay(0.005);		// wait for a motor update time	
+         }
+     	
+    	
     	Double SpeedControal = 4.0;
     	if(left.get()){
     		motor_left.set(joy.getRawAxis(1)/SpeedControal);                     	
