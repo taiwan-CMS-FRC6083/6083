c1dc388591b4e71486c537486a92df5b3abc2bb9
zacky <aszx50@kimo.com>
2016-01-24 14:29:09 +0800

    test

--- 6083v1/src/org/usfirst/frc/team6083/robot/Robot.java
+++ 6083v1/src/org/usfirst/frc/team6083/robot/Robot.java
@@ -41,6 +41,7 @@ public class Robot extends IterativeRobot {
     Double RY;
     
     
+    
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
