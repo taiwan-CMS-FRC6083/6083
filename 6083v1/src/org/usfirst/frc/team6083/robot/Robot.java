9f94ff3ecdafed9da00493d3a0f9f82bbb3e1056
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2016-01-27 20:36:39 +0800

    整理CODE和新增Dashboard

--- 6083v1/src/org/usfirst/frc/team6083/robot/Robot.java
+++ 6083v1/src/org/usfirst/frc/team6083/robot/Robot.java
@@ -6,10 +6,14 @@ import com.ni.vision.NIVision.Image;
 import com.ni.vision.NIVision.ShapeMode;
 
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.Preferences;
 
 
 /**
@@ -21,11 +25,26 @@ import edu.wpi.first.wpilibj.Joystick;
 public class Robot extends SampleRobot {
     int session;
     Image frame;
+    
+    //motor
     VictorSP talon_left = new VictorSP(0);
     VictorSP talon_right = new VictorSP(1);
+    		
+    //joystick
     Joystick joy = new Joystick(0);
     
+    //SmartDashboard
+    Preferences pref;
     
+    //Device
+    PowerDistributionPanel pdp = new PowerDistributionPanel(1);
+    Compressor comp = new Compressor(0);
+    
+    //Double
+    Double LY;
+    Double RY;
+    
+
     
 
     public void robotInit() {
@@ -57,21 +76,29 @@ public class Robot extends SampleRobot {
 
             /** robot code here! **/
             Timer.delay(0.005);// wait for a motor update time
-            
-            if(joy.getRawButton(5)){
-         	   talon_left.set(joy.getRawAxis(1)/-2.5);
-         	}
-         	else{
-         		talon_left.set(joy.getRawAxis(1)/-5);
-         	}
-         	
-         	if(joy.getRawButton(6)){
-         	    talon_right.set(joy.getRawAxis(5)/2.5);
-         	}
-         	else{ 
-         		talon_right.set(joy.getRawAxis(5)/5);
-         	}
-         	
+            if(pdp.getVoltage()<=7){
+                if(joy.getRawButton(5)){
+              	   talon_left.set(joy.getRawAxis(1)/-2.5);
+              	}
+              	else{
+              		talon_left.set(joy.getRawAxis(1)/-5);
+              	}
+              	
+              	if(joy.getRawButton(6)){
+              	    talon_right.set(joy.getRawAxis(5)/2.5);
+              	}
+              	else{ 
+              		talon_right.set(joy.getRawAxis(5)/5);
+              	}
+            }
+
+        	SmartDashboard.putNumber("Left Motor Encoder Value", -talon_left.get());
+        	SmartDashboard.putNumber("Right Motor Encoder Value", talon_right.get());
+        	SmartDashboard.putNumber("spSpeedControaleed", (-talon_left.get()+ talon_right.get())/2);
+        	SmartDashboard.putNumber("Speed Plot", (-talon_left.get()+ talon_right.get())/2);
+        	SmartDashboard.putNumber("LY value", joy.getRawAxis(1));
+        	SmartDashboard.putNumber("RY value", joy.getRawAxis(5));
+        	SmartDashboard.putNumber("PDP Voltage", pdp.getTotalPower());
          	
             
             
