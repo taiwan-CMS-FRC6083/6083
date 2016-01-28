6c608843dba96f020973f8750a24cc73ffd7e782
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2016-01-28 19:45:34 +0800

    更新Camera

--- 6083v1/src/org/usfirst/frc/team6083/robot/Robot.java
+++ 6083v1/src/org/usfirst/frc/team6083/robot/Robot.java
@@ -1,64 +1,115 @@
+
 package org.usfirst.frc.team6083.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.ni.vision.NIVision;
 import com.ni.vision.NIVision.DrawMode;
 import com.ni.vision.NIVision.Image;
 import com.ni.vision.NIVision.ShapeMode;
 
-import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.VictorSP;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
-import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.vision.USBCamera;
+
 
 
 /**
- * This is a demo program showing the use of the NIVision class to do vision processing. 
- * The image is acquired from the USB Webcam, then a circle is overlayed on it. 
- * The NIVision class supplies dozens of methods for different types of processing. 
- * The resulting image can then be sent to the FRC PC Dashboard with setImage()
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
  */
-public class Robot extends SampleRobot {
-    int session;
-    Image frame;
+public class Robot extends IterativeRobot {
+    final String defaultAuto = "Default";
+    final String customAuto = "My Auto";
+    String autoSelected;
+    SendableChooser chooser;
+    
     
     //motor
-    VictorSP talon_left = new VictorSP(0);
-    VictorSP talon_right = new VictorSP(1);
-    		
-    //joystick
-    Joystick joy = new Joystick(0);
+    VictorSP motor_left = new VictorSP(1);
+    VictorSP motor_right = new VictorSP(0);
     
-    //SmartDashboard
-    Preferences pref;
+    //Joystick
+    Joystick joy = new Joystick(0);
+    JoystickButton left = new JoystickButton(joy,9);
+    JoystickButton right = new JoystickButton(joy,10);
     
     //Device
     PowerDistributionPanel pdp = new PowerDistributionPanel(1);
     Compressor comp = new Compressor(0);
     
+    //SmartDashboard
+    Preferences pref;
+    
+    //camera
+    int session;
+    Image frame;
+    
     //Double
     Double LY;
     Double RY;
     
-
     
-
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
     public void robotInit() {
-
+        chooser = new SendableChooser();
+        chooser.addDefault("Default Auto", defaultAuto);
+        chooser.addObject("My Auto", customAuto);
+        SmartDashboard.putData("Auto choices", chooser);
+        
+        
         frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
 
         // the camera name (ex "cam0") can be found through the roborio web interface
         session = NIVision.IMAQdxOpenCamera("cam0",
                 NIVision.IMAQdxCameraControlMode.CameraControlModeController);
         NIVision.IMAQdxConfigureGrab(session);
+        
+    }
+    
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 */
+    public void autonomousInit() {
+    	autoSelected = (String) chooser.getSelected();
+//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
+		System.out.println("Auto selected: " + autoSelected);
     }
 
-    public void operatorControl() {
-        NIVision.IMAQdxStartAcquisition(session);
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    	switch(autoSelected) {
+    	case customAuto:
+        //Put custom auto code here   
+            break;
+    	case defaultAuto:
+    	default:
+    	//Put default auto code here
+            break;
+    	}
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+
+    	NIVision.IMAQdxStartAcquisition(session);
 
         /**
          * grab an image, draw the circle, and provide it for the camera server
@@ -75,38 +126,52 @@ public class Robot extends SampleRobot {
             CameraServer.getInstance().setImage(frame);
 
             /** robot code here! **/
-            Timer.delay(0.005);// wait for a motor update time
-            if(pdp.getVoltage()<=7){
-                if(joy.getRawButton(5)){
-              	   talon_left.set(joy.getRawAxis(1)/-2.5);
-              	}
-              	else{
-              		talon_left.set(joy.getRawAxis(1)/-5);
-              	}
-              	
-              	if(joy.getRawButton(6)){
-              	    talon_right.set(joy.getRawAxis(5)/2.5);
-              	}
-              	else{ 
-              		talon_right.set(joy.getRawAxis(5)/5);
-              	}
-            }
+        	Double SpeedControal = 4.0;
+        	
+        	if(joy.getRawAxis(1)>0.1 || joy.getRawAxis(1)<-0.1){		
+                LY = joy.getRawAxis(1);
+            }	
+            else{
+                LY = 0.0 ;	
+            }	
+        	if(joy.getRawAxis(5)>0.1 || joy.getRawAxis(5)<-0.1){		
+                RY = joy.getRawAxis(5);
+            }	
+            else{
+                RY = 0.0 ;	
+            }	
+        	if(left.get()){
+        		motor_left.set(LY/SpeedControal);                     	
+        	}	
+        	else {
+        		motor_left.set(LY/(SpeedControal*2));
+        	}
+        	
+        	if(right.get()){
+        		motor_right.set(-RY/SpeedControal);                     	
+        	}	
+        	else {
+        		motor_right.set(-RY/(SpeedControal*2));
+        	}
 
-        	SmartDashboard.putNumber("Left Motor Encoder Value", -talon_left.get());
-        	SmartDashboard.putNumber("Right Motor Encoder Value", talon_right.get());
-        	SmartDashboard.putNumber("spSpeedControaleed", (-talon_left.get()+ talon_right.get())/2);
-        	SmartDashboard.putNumber("Speed Plot", (-talon_left.get()+ talon_right.get())/2);
+        	SmartDashboard.putNumber("Left Motor Encoder Value", -motor_left.get());
+        	SmartDashboard.putNumber("Right Motor Encoder Value", motor_right.get());
+        	SmartDashboard.putNumber("spSpeedControaleed", (-motor_left.get()+ motor_right.get())/2);
+        	SmartDashboard.putNumber("Speed Plot", (-motor_left.get()+ motor_right.get())/2);
         	SmartDashboard.putNumber("LY value", joy.getRawAxis(1));
         	SmartDashboard.putNumber("RY value", joy.getRawAxis(5));
         	SmartDashboard.putNumber("PDP Voltage", pdp.getVoltage());
-         	
-            
-            
-            
-        } 
+            Timer.delay(0.005);		// wait for a motor update time
+        }
         NIVision.IMAQdxStopAcquisition(session);
+    	
     }
-
-    public void test() {
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    
     }
+    
 }
